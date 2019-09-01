5c1b53fd22a00e06d38bfdc4dddd93fd99c6a5b4
kurolight <yongfatan@gmail.com>
2019-09-01 14:07:57 +0800

    bert serving no pooling, show tokens

--- Bert Playground/playground.py
+++ Bert Playground/playground.py
@@ -1,12 +1,29 @@
 from bert_serving.client import BertClient
 from pprint import pprint
+from sklearn.metrics.pairwise import cosine_similarity
 
 # print('something!')
 with BertClient() as bc:  # port=5555, port_out=5556
     # print('test')
     # pprint(bc.status)
     # pprint(bc.server_status)
-    result = bc.encode(['First do it', 'then do it right', 'then do it better'])
+
+    # result = bc.encode(['First do it', 'then do it right', 'then do it better'])
+    # print(result)
+    # result = bc.encode(['First do it ||| then do it right'])
+    # print(result)
+
+    result = bc.encode(['猫吃什么', '狗吃什么'])
+    # print(result.shape)
+    pprint(result[0])
+    sim = cosine_similarity(result[0].reshape(1, -1), result[1].reshape(1, -1))
+    print(sim)
+
+    result = bc.encode(['猫吃什么', '猫做什么'], show_tokens=True)
+    # print(result.shape)
+    # sim = cosine_similarity(result[0].reshape(1, -1), result[1].reshape(1, -1))
+    # print(sim)
     print(result)
-    result = bc.encode(['First do it ||| then do it right'])
+
+    result = bc.encode(['猫吃什么 ||| 猫做什么'], show_tokens=True)
     print(result)
--- Bert Playground/start.sh
+++ Bert Playground/start.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-bert-serving-start -model_dir '../../bert_model/chinese_wwm_ext_L-12_H-768_A-12' -num_worker=4
+bert-serving-start \
+    -model_dir '../../bert_model/chinese_wwm_ext_L-12_H-768_A-12' \
+    -num_worker 4 \
+    -pooling_strategy NONE \
+    -show_tokens_to_client
